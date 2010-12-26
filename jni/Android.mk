e3d6b0c776017e612faa0fc5454fba62a739ae00
Steven Lin <steven.lin@raycommtech.com>
2010-12-26 14:17:25 +0800

    first release

---
+++ jni/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/src
+
+LOCAL_SRC_FILES := \
+	com_android_inputmethod_latin_BinaryDictionary.cpp \
+	src/dictionary.cpp \
+	src/char_utils.cpp
+
+LOCAL_NDK_VERSION := 4
+LOCAL_SDK_VERSION := 8
+
+LOCAL_MODULE := libjni_latinime
+
+include $(BUILD_SHARED_LIBRARY)
